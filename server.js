f8c64c803f510f191767403404377e832ee0a7d1
Biplop Maharjan <maharjankija@gmail.com>
2026-06-17 09:25:45 +0545

    secondary update in lock package and server

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const pool = new Pool({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use('/uploads', express.static('uploads'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
